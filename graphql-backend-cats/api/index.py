ba1654d5ee637464659f56fd935dec900f8b0633
Oscarvm117 <oscarvemo@unisabana.edu.co>
2025-10-28 15:00:23 -0500

    updates index.py for vercel

--- graphql-backend-cats/api/index.py
+++ graphql-backend-cats/api/index.py
@@ -1,10 +1,5 @@
 import sys
-import os
-
-# Agregar el directorio raíz al path
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-
+from mangum import Mangum
 from main import app
 
-# Vercel espera que la app se llame 'app' o se exporte como handler
-handler = app
+handler = Mangum(app)
